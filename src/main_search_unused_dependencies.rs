60b5f8855c37b2a09a4dfe2214160d9e6e6e0a8b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-05-07 18:54:10 +0200

    Make the program work as intended.

--- src/main_search_unused_dependencies.rs
+++ src/main_search_unused_dependencies.rs
@@ -3,8 +3,28 @@ use cargo_toml::Manifest;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
-use std::collections::{BTreeMap, BTreeSet};
-use cargo_toml::Dependency;
+use std::process::Command;
+
+
+fn read_lines<P>(filename: P) -> io::Result<String>
+where
+    P: AsRef<Path>,
+{
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename)?;
+
+    // Create a new BufReader, which will allow us to read the file line by line.
+    let reader = BufReader::new(file);
+
+    // Collect the lines of the file into a Vec<String>.
+    let lines: Vec<String> = reader.lines()
+        .collect::<Result<_, _>>()?;
+    let mut entry = String::new();
+    for line in lines {
+        entry += &line;
+    }
+    Ok(entry)
+}
 
 
 fn main() {
@@ -29,7 +49,50 @@ fn main() {
 
     let manifest = Manifest::from_path(cargo_toml).expect("obtain a manifest");
 
-    println!("manifest={manifest:?}");
+//    println!("manifest={manifest:?}");
+//    println!();
 
+    let mut dependencies = Vec::new();
+    for (key, _value) in manifest.dependencies {
+        let dependency: String = key.replace("-", "_").to_string();
+        dependencies.push(dependency);
+    }
+//    println!("dependencies={dependencies:?}");
+//    println!();
+
+    let output = Command::new("find")
+        .arg(".")
+        .current_dir(&directory)
+        .output()
+        .expect("Failed to execute command");
+
+//    println!("output={output:?}");
+    let stdout = String::from_utf8(output.stdout).unwrap();
+//    println!("stdout={:?}", stdout);
+
+    let mut files = Vec::new();
+    for entry in stdout.split("\n") {
+        if entry.ends_with(".rs") {
+            files.push(entry);
+        }
+    }
+//    println!("files={files:?}");
+
+    let mut full_string = String::new();
+    for file in files {
+        let full_file = format!("{directory}/{file}");
+        let the_read = read_lines(full_file).expect("Reading full_file={full_file}");
+        full_string += &the_read;
+    }
+
+    let mut n_found = 0;
+    for dependency in dependencies {
+        let l_vec = full_string.split(&dependency).collect::<Vec<_>>();
+        if l_vec.len() == 1 {
+            println!("dependency={dependency} may be not needed");
+            n_found += 1;
+        }
+    }
+    println!("n_found={n_found}");
 }
 
